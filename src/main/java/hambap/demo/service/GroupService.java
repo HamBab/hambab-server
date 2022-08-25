27ea2e891b48a795f41e8b5db666c71e468864ac
leta-ha <letaha@naver.com>
2022-08-25 19:24:25 +0900

    Feat: 그룹 삭제 기능

--- src/main/java/hambap/demo/service/GroupService.java
+++ src/main/java/hambap/demo/service/GroupService.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 public interface GroupService {
 
     String updateGroup(String name, Long id);
+    String deleteGroup(Long id);
     Optional<List<Group>> findAllGroups();
     Optional<Group> createGroup(Long restaurantId, Long leaderId);
     Optional<Group> findGroupById(Long id);
